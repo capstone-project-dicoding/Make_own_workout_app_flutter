e7f9ba607b367fcce14092b916c9c20cae640d7e
mfajrihusaini02 <mfajrihusaini05@gmail.com>
2022-07-03 21:19:35 +0700

    integration provider

---
+++ lib/data/db/db_helper.dart
@@ -0,0 +1,78 @@
+import 'package:make_own_workout/model/mow_model.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBHelper {
+  static DBHelper? _instance;
+  static Database? _database;
+
+  DBHelper._internal() {
+    _instance = this;
+  }
+
+  factory DBHelper() => _instance ?? DBHelper._internal();
+
+  static const String _tblApp = 'mow';
+
+  Future<Database> _initializeDb() async {
+    var path = await getDatabasesPath();
+    var db = openDatabase(
+      '$path/mow_app.db',
+      onCreate: (db, version) async {
+        await db.execute('''CREATE TABLE $_tblApp (
+             id TEXT PRIMARY KEY,
+             name TEXT,
+             image TEXT,
+             description TEXT
+           )     
+        ''');
+      },
+      version: 1,
+    );
+
+    return db;
+  }
+
+  Future<Database?> get database async {
+    _database ??= await _initializeDb();
+
+    return _database;
+  }
+
+  Future<void> insertMOW(MOWModel mow) async {
+    final db = await database;
+    await db!.insert(_tblApp, mow.toJson());
+  }
+
+  Future<List<MOWModel>> getAlls() async {
+    final db = await database;
+    List<Map<String, dynamic>> results = await db!.query(_tblApp);
+
+    return results.map((mow) => MOWModel.fromJson(mow)).toList();
+  }
+
+  Future<Map> getMOWById(String id) async {
+    final db = await database;
+
+    List<Map<String, dynamic>> results = await db!.query(
+      _tblApp,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (results.isNotEmpty) {
+      return results.first;
+    } else {
+      return {};
+    }
+  }
+
+  Future<void> removeMOW(String id) async {
+    final db = await database;
+
+    await db!.delete(
+      _tblApp,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
---
+++ lib/provider/db_provider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:make_own_workout/data/db/db_helper.dart';
+import 'package:make_own_workout/model/mow_model.dart';
+import 'package:make_own_workout/provider/mow_provider.dart';
+
+class DatabaseProvider extends ChangeNotifier {
+  final DBHelper dbHelper;
+
+  DatabaseProvider({required this.dbHelper}) {
+    _getAlls();
+  }
+
+  ResultState? _state;
+  ResultState? get state => _state;
+
+  String _message = '';
+  String get message => _message;
+
+  List<MOWModel> _mow = [];
+  List<MOWModel> get mow => _mow;
+
+  void _getAlls() async {
+    _mow = await dbHelper.getAlls();
+    if (_mow.length > 0) {
+      _state = ResultState.hasData;
+    } else {
+      _state = ResultState.noData;
+      _message = 'Empty Data';
+    }
+    notifyListeners();
+  }
+
+  void addMOW(MOWModel mow) async {
+    try {
+      await dbHelper.insertMOW(mow);
+      _getAlls();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
+  }
+
+  Future<bool> isFavorited(String id) async {
+    final favoritedMOW = await dbHelper.getMOWById(id);
+    return favoritedMOW.isNotEmpty;
+  }
+
+  void removeMOW(String id) async {
+    try {
+      await dbHelper.removeMOW(id);
+      _getAlls();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
+  }
+}
